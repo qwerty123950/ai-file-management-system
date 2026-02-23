649c0c8daf585c2191f47234b630f7d4736f8297
qwerty123950 <ben.kattackan123@gmail.com>
2026-02-23 13:03:17 +0530

    minor debugging

--- test_groq.py
+++
@@ -1,9 +0,0 @@
-# test_groq.py
-
-from backend.services.chat_service import groq_chat
-
-doc = "Apache Pig is a high-level platform for creating MapReduce programs."
-instruction = "Summarize this in 20 words."
-
-print(groq_chat(doc, instruction))
-
